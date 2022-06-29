dd06bafe28e667f0566779f6f0780a2babfbfb23
Prithvi <prithvisinghtewatia@gmail.com>
2022-06-29 09:35:13 +0530

    Initialized repository

---
+++ src/test/java/com/claimsmanagement/claimsmanagement/ClaimsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claimsmanagement.claimsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
