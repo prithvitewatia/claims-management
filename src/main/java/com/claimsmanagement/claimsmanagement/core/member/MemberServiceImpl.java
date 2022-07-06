86bfde95003414f0ba6de13f237fa275c90594a8
Prithvi <prithvisinghtewatia@gmail.com>
2022-07-06 15:02:27 +0530

    Split service into impl and interface

--- src/main/java/com/claimsmanagement/claimsmanagement/core/member/MemberServiceImpl.java
+++ src/main/java/com/claimsmanagement/claimsmanagement/core/member/MemberServiceImpl.java
@@ -1,7 +1,7 @@
 package com.claimsmanagement.claimsmanagement.core.member;
 
 import com.claimsmanagement.claimsmanagement.core.member.converter.MemberToMemberViewConverter;
-import com.claimsmanagement.claimsmanagement.core.member.web.MemberBaseRequest;
+import com.claimsmanagement.claimsmanagement.core.member.web.MemberRequest;
 import com.claimsmanagement.claimsmanagement.core.member.web.MemberView;
 import com.claimsmanagement.claimsmanagement.error.EntityNotFoundException;
 import com.claimsmanagement.claimsmanagement.util.MessageUtil;
@@ -18,7 +18,7 @@ import java.util.List;
 
 
 @Service
-public class MemberService {
+public class MemberServiceImpl implements MemberService{
     @Autowired
     private MemberRepo memberRepo;
     @Autowired
@@ -46,15 +46,19 @@ public class MemberService {
         return new PageImpl<>(memberViews,pageable,members.getTotalElements());
     }
 
-    public MemberView create(MemberBaseRequest memberBaseRequest){
+    public MemberView create(MemberRequest memberRequest){
         Member member=new Member();
-        this.prepare(member,memberBaseRequest);
+        this.prepare(member,memberRequest);
         Member memberSave=memberRepo.save(member);
         return memberViewConverter.convert(memberSave);
     }
 
-    private void prepare(Member member, MemberBaseRequest memberBaseRequest) {
-        // TODO : complete prepare function in member service
+    private void prepare(Member member, MemberRequest memberRequest) {
+        member.setMemberName(memberRequest.getMemberName());
+        member.setEmail(memberRequest.getEmail());
+        member.setPassword(memberRequest.getPassword());
+        member.setPhoneNo(memberRequest.getPhoneNo());
+        member.setAddress(memberRequest.getAddress());
     }
 
     @Transactional
@@ -65,8 +69,8 @@ public class MemberService {
             throw new EntityNotFoundException(messageUtil.getMessage("member.NotFound",id));
         }
     }
-    public MemberView update(Member member,MemberBaseRequest memberBaseRequest){
-        this.prepare(member,memberBaseRequest);
+    public MemberView update(Member member, MemberRequest memberRequest){
+        this.prepare(member,memberRequest);
         Member saveMember=memberRepo.save(member);
         return memberViewConverter.convert(saveMember);
     }
