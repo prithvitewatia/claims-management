c218506e3a55896ecb1937ec165cd819c1407ec3
Prithvi <prithvisinghtewatia@gmail.com>
2022-07-06 15:01:26 +0530

    Created member controller

--- src/main/java/com/claimsmanagement/claimsmanagement/core/member/web/MemberController.java
+++ src/main/java/com/claimsmanagement/claimsmanagement/core/member/web/MemberController.java
@@ -17,31 +17,35 @@ import javax.validation.Valid;
 public class MemberController {
     @Autowired
     private MemberService memberService;
+
     @RequestMapping("/{id}")
-    public MemberView getMemberById(@PathVariable("id") Long id){
+    public MemberView getMemberById(@PathVariable("id") Long id) {
         return memberService.getMemberById(id);
     }
+
     @GetMapping
     public Page<MemberView> getAllMembers(@PageableDefault(sort = "memberId",
-            direction = Sort.Direction.DESC)Pageable pageable){
+            direction = Sort.Direction.DESC) Pageable pageable) {
         return memberService.getAllMembers(pageable);
     }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MemberView create(@RequestBody @Valid MemberBaseRequest memberBaseRequest){
-        return memberService.create(memberBaseRequest);
+    public MemberView create(@RequestBody @Valid MemberRequest memberRequest) {
+        return memberService.create(memberRequest);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteMember(@PathVariable("id") Long id){
+    public void deleteMember(@PathVariable("id") Long id) {
         memberService.delete(id);
     }
+
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public MemberView updateMember(@PathVariable("id") Long id
-            ,@RequestBody @Valid MemberBaseRequest memberBaseRequest){
-        Member member=memberService.findMemberOrThrow(id);
-        return memberService.update(member,memberBaseRequest);
+            , @RequestBody @Valid MemberRequest memberRequest) {
+        Member member = memberService.findMemberOrThrow(id);
+        return memberService.update(member, memberRequest);
     }
 }
