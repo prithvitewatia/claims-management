a8b8e03768f9ba616f027f2a46226141a4b57298
Prithvi <prithvisinghtewatia@gmail.com>
2022-07-06 19:15:47 +0530

    Deleted Member base request

--- src/main/java/com/claimsmanagement/claimsmanagement/core/member/web/MemberBaseRequest.java
+++
@@ -1,7 +0,0 @@
-package com.claimsmanagement.claimsmanagement.core.member.web;
-
-import com.claimsmanagement.claimsmanagement.base.BaseRequest;
-
-public class MemberBaseRequest extends BaseRequest {
-    // TODO : add request attributes
-}
