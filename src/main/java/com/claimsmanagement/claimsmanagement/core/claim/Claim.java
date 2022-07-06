22f1102b15adfdf4cf9d5bf4ced61518c2cd95fc
Prithvi <prithvisinghtewatia@gmail.com>
2022-07-06 15:00:14 +0530

    Created claim entity

--- src/main/java/com/claimsmanagement/claimsmanagement/core/claim/Claim.java
+++ src/main/java/com/claimsmanagement/claimsmanagement/core/claim/Claim.java
@@ -1,4 +1,53 @@
 package com.claimsmanagement.claimsmanagement.core.claim;
 
+import com.claimsmanagement.claimsmanagement.core.policy.Policy;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Claim {
+    @Id
+    @Column
+    @GenericGenerator(
+            name = "claim_id_seq",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name="sequence_name",value="claim_id_seq"),
+                    @org.hibernate.annotations.Parameter(name="INCREMENT",value = "1"),
+                    @org.hibernate.annotations.Parameter(name="MINVALUE", value = "1"),
+                    @org.hibernate.annotations.Parameter(name="MAXVALUE", value = "2147483647"),
+                    @org.hibernate.annotations.Parameter(name = "CACHE", value = "1")
+            }
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "claim_id_seq")
+    private Long claimId;
+    @Column(length = 1000)
+    private String description; // Submitted by User
+    @Column(nullable = false)
+    private LocalDate claimRaisedDate;
+    private LocalDate claimSettledDate;
+    @Column(nullable = false)
+    private Integer claimAmount;
+    @Column(nullable = false)
+    private String claimStatus;
+    @Column(length = 1000)
+    private String remarks; // Submitted by Admin/Approving Person
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "policyId",nullable = false)
+    private Policy policy;
 }
